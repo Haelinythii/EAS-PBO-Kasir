742b52e729a533b2d429ee7dec8ef57cd9752dca
Haelinythii <47884588+Haelinythii@users.noreply.github.com>
2019-12-12 11:57:07 +0700

    Add files via upload

---
+++ PrintReceipt.java
@@ -0,0 +1,21 @@
+
+import javafx.print.PrinterJob;
+import javafx.scene.Node;
+
+public class PrintReceipt
+{
+    public void printBuktiTransaksi(Node node)
+    {
+        PrinterJob printJob = PrinterJob.createPrinterJob();
+        if(printJob == null)
+        {
+            return;
+        }
+        else
+        {
+            boolean printStatus = printJob.printPage(node);
+            if (printStatus == true) printJob.endJob();
+        }
+    }
+    
+}
